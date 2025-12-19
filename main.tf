54708f590eb10816bbca52331cc22bab9aaba96a
tmerge <til.merge@live.de>
2025-12-19 18:58:39 +0100

    complete my first name as stone blocks

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  t_start = []
+}
+
 resource "minecraft_block" "single_block" {
   material = var.material
 
@@ -10,4 +14,75 @@ resource "minecraft_block" "single_block" {
 
 resource "minecraft_gamemode" "gamemode" {
   mode = var.gamemode
+}
+
+resource "minecraft_fill" "T" {
+  material = var.material
+  start = {
+    x = 0,
+    y = 0,
+    z = 0,
+  }
+  end = {
+    x = 4,
+    y = 10,
+    z = 4,
+  }
+}
+
+resource "minecraft_fill" "T2" {
+  depends_on = [ minecraft_fill.T ]
+  material = var.material
+  start = {
+    x = 0,
+    y = 11,
+    z = 0,
+  }
+  end = {
+    x = 5,
+    y = 11,
+    z = 4,
+  }
+}
+
+resource "minecraft_fill" "I" {
+  material = var.material
+  start = {
+    x = 6,
+    y = 0,
+    z = 0
+  }
+  end = {
+    x = 10,
+    y = 11
+    z = 4
+  }
+}
+
+resource "minecraft_fill" "L" {
+  material = var.material
+  start = {
+    x = 12,
+    y = 0,
+    z = 0
+  }
+  end = {
+    x = 12,
+    y = 11,
+    z = 4
+  }
+}
+
+resource "minecraft_fill" "L2" {
+  material = var.material
+  start = {
+    x = 13,
+    y = 0,
+    z = 0
+  }
+  end = {
+    x = 21,
+    y = 1,
+    z = 4
+  }
 }
